c3ee731e83113f91fcd39ec502e1d9f9d8527686
Tiffany Manolis <tiffany.mano@gmail.com>
2021-11-01 20:06:31 +0100

    boucle de split and push OK

--- srcs/algorithm/big_args_set/sort_big_set.c
+++ srcs/algorithm/big_args_set/sort_big_set.c
@@ -4,7 +4,13 @@ void    display_stack(t_data *data);
 
 void	sort_big_set(t_data *data)
 {
-	split_and_push(data);
-    display_stack(data);
-    split_and_push(data);
+    size_t len;
+
+    len = ft_lstsize(data->lst_a);
+    while (len > 3)
+    {
+        split_and_push(data);
+        display_stack(data);
+        len = ft_lstsize(data->lst_a);
+    }
 }
