52e1d7a25a4cee8d0e3a950a4d89a5317f51b7e9
Tiffany MANOLIS <tmanolis@e1r1p8.42.fr>
2021-11-23 12:31:25 +0100

    proximity

--- srcs/algorithm/big_args_set/proximity.c
+++ srcs/algorithm/big_args_set/proximity.c
@@ -12,3 +12,32 @@
 
 #include "../../../push_swap.h"
 
+int find_index(long int number, t_list *lst)
+{
+	int     index;
+	t_list  *tmp;
+
+	index = 0;
+	tmp = lst;
+	while (tmp)
+	{
+		if (tmp->content == number)
+			return (index);
+		tmp = tmp->next;
+		index++;
+	}
+	return (index);
+}
+
+void	proximity(long int number, t_data *data)
+{
+	int	index;
+	int	proximity;
+
+	index = find_index(number, data->lst_a);
+	proximity = (ft_lstsize(data->lst_a)) / 2;
+	if (proximity > index)
+		rotate_a(data);
+	else
+		reverse_rotate_a(data);
+}
